080c88be07451da2412d125fd9954356541af611
HaseebTariq1998 <haseebmehboob1998@gmail.com>
2023-11-09 21:27:26 +0500

    [ backend ] Resolved error check issue in onWeatherDataReceived function.

--- CarDashHMI/Weather/weather.cpp
+++ CarDashHMI/Weather/weather.cpp
@@ -66,7 +66,7 @@ void Weather::fetchWeatherData()
 
 void Weather::onWeatherDataReceived(QNetworkReply *reply)
 {
-    if (reply->error() != QNetworkReply::NoError)
+    if (reply->error() == QNetworkReply::NoError)
     {
         QByteArray data = reply->readAll();
         QJsonDocument jsonDoc = QJsonDocument::fromJson(data);
